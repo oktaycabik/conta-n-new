2db165152c89a71fe3f8f909934c4ba7cda36d9c
ContainHaus <mybook@192.168.1.109>
2022-08-23 09:25:42 +0300

    image etiket

--- components/CategoryProducts/ProductsImage.js
+++ components/CategoryProducts/ProductsImage.js
@@ -1,28 +1,34 @@
 import React from 'react'
-
+import Image from 'next/image'
 const ProductsImage = ({pro}) => {
   return (
     <div className="row">
     <div className="col-lg-4 col-12 mt-2">
-      <img
+      <Image
         className="w-100 product-img"
-      
+        width={400}
+        height={275}
+        objectFit='cover'
         src={pro.image.ima5}
         alt=""
       />
     </div>
     <div className="col-lg-4 col-6 mt-2">
-      <img
+      <Image
         className="w-100 product-img"
-    
+        width={400}
+        height={275}
+        objectFit='cover'
         src={pro.image.ima4}
         alt=""
       />
     </div>
     <div className="col-lg-4 col-6 mt-2">
-      <img
+      <Image
         className="w-100 product-img"
-       
+        objectFit='cover'
+        width={400}
+        height={275}
         src={pro.image.ima3}
         alt=""
       />
