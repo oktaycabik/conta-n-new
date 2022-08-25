af962e8b9ff146c79a2989eda43a80c322d5911c
ContainHaus <mybook@192.168.1.109>
2022-08-25 10:12:49 +0300

    image fixed

--- components/CategoryProducts/ProductsImage.js
+++ components/CategoryProducts/ProductsImage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 const ProductsImage = ({pro}) => {
+  const myLoader = ({ src, width, quality }) => {
+    return `${pro.image.ima5}?w=${400}&q=${quality || 85}&?fm=webp`  };
+    const myLoader1 = ({ src, width, quality }) => {
+      return `${pro.image.ima4}?w=${400}&q=${quality || 85}&?fm=webp`  };
+      const myLoader2 = ({ src, width, quality }) => {
+        return `${pro.image.ima3}?w=${400}&q=${quality || 85}&?fm=webp`  };
   return (
     <div className="row pl-5 pr-5">
     <div className="col-lg-4 col-12 p-1 mt-2 ">
       <Image
-      
+      priority
+      loader={myLoader}
         width={400}
         height={275}
         objectFit='cover'
@@ -15,7 +22,8 @@ const ProductsImage = ({pro}) => {
     </div>
     <div className="col-lg-4 col-6 p-1 mt-2">
       <Image
-        
+       priority
+        loader={myLoader1}
         width={400}
         height={275}
         objectFit='cover'
@@ -25,7 +33,8 @@ const ProductsImage = ({pro}) => {
     </div>
     <div className="col-lg-4 col-6  p-1 mt-2">
       <Image
-      
+       priority
+      loader={myLoader2}
         objectFit='cover'
         width={400}
         height={275}
--- components/ProductPage/ProductFeatures.js
+++ components/ProductPage/ProductFeatures.js
@@ -4,23 +4,23 @@ const ProductFeatures = ({product}) => {
   return (
     <div className="container">
       <div className="row mt-5 pr-5 pl-5 justify-content-center">
-        <div className="col-5 col-12 ">
+        <div className="col-lg-5 col-12 ">
             <img className="w-100" src="https://conliving.de/wp-content/uploads/2020/03/DoppelWaschbecken.jpg"/>
         </div>
-        <div className="col-5 col-12 mt-2 text-center my-auto">3x Doppel-Waschbecken</div>
+        <div className="col-lg-5 col-12 mt-2 text-center my-auto">3x Doppel-Waschbecken</div>
       </div>
       <div className="row mt-5 pr-5 pl-5 justify-content-center img-reverse">
-      <div className="col-5 col-12 mt-2 text-center my-auto ">3x Doppel-Waschbecken</div>
-        <div className="col-5 col-12 ">
+      <div className="col-lg-5 col-12 mt-2 text-center my-auto ">3x Doppel-Waschbecken</div>
+        <div className="col-lg-5 col-12 ">
             <img className="w-100" src="https://conliving.de/wp-content/uploads/2017/01/Bad-toilette-dusche-waschbecken.jpg"/>
         </div>
        
       </div>
       <div className="row mt-5 pr-5 pl-5 justify-content-center">
-        <div className="col-5 col-12">
+        <div className="col-lg-5 col-12">
             <img className="w-100" src="https://conliving.de/wp-content/uploads/2020/04/SC-Toilette-Einzel.jpg"/>
         </div>
-        <div className="col-5 col-12 mt-2 text-center my-auto">3x Doppel-Waschbecken</div>
+        <div className="col-lg-5 col-12 mt-2 text-center my-auto">3x Doppel-Waschbecken</div>
       </div>
     </div>
   );
