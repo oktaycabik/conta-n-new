60069241040c20bfc8eceb4e75ac521734960de1
Oktay Çabik <oktaycabik2@gmail.com>
2022-09-01 23:17:07 +0300

    test

--- pages/referenzen.js
+++ pages/referenzen.js
@@ -1,6 +1,15 @@
 import React from "react";
 import referenzen from "../data/referenzen.json";
+import Image from "next/image";
 const Referenzen = () => {
+  const cloudflareImagesLoader = ({ src, width }) => {
+    // Note that `width` might be larger than you're expecting because of a device pixel ratio (DPR)
+
+    // Next.js expects to see the width somewhere in the URL,
+    // so we add the no-op `width` query parameter to suppress the warning
+    // https://nextjs.org/docs/messages/next-image-missing-loader-width
+    return `https://imagedelivery.net/Hd6PfXQgWuxwOTKaoptnYw/${src}/public`;
+  };
   return (
     <div className="container pl-5 pr-5">
       <div className="col-12">
@@ -14,7 +23,7 @@ const Referenzen = () => {
         {referenzen.map((refe) => (
           <div key={refe.id} className="col-lg-6 col-12">
             <div className="border mb-3">
-              <img className="w-100 shadow" src={refe.image.ima1} />
+              <img className="w-100 shadow"  src={refe.image.ima1} />
               <h3 className="referenzen-title mt-3 ms-3">{refe.title}</h3>
 
               <button className="mt-1 button-dark-small mb-4 ms-3">
